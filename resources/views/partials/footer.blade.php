c5df649b7cace28fa84391faf7ef5b23cc48b5a9
mikhailidi <slipknot1.ru>
2018-01-20 20:06:06 +0100

    Clean up components, split layout to partials

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container">
+        <div class="content has-text-centered">
+            <p>
+                <strong>Project is created</strong> by <a href="https://github.com/mikhailidi">Pavlo Mikhailidi</a>
+            </p>
+            <p>
+                <a class="icon" href="https://github.com/mikhailidi/Laravel-chat-with-Vue">
+                    <i class="fa fa-github"></i>
+                </a>
+            </p>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/left.blade.php
@@ -0,0 +1,17 @@
+<aside class="column is-2 aside hero is-fullheight">
+    <div>
+        <div class="compose has-text-centered">
+            <a class="button is-danger is-block is-bold">
+                <span class="compose">
+                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                    New message
+                </span>
+            </a>
+        </div>
+        <div class="main">
+            <a href="#" class="item active"><span class="icon"><i class="fa fa-envelope-o"></i></span><span class="name">Chat</span></a>
+            <a href="#" class="item"><span class="icon"><i class="fa fa-user"></i></span><span class="name">Profile</span></a>
+            <a href="#" class="item"><span class="icon"><i class="fa fa-cog"></i></span><span class="name">Settings</span></a>
+        </div>
+    </div>
+</aside>
