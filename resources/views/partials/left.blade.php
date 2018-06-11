37b3a176b6dcf830e7855edad358ff07ad3e368c
mikhailidi <mikhailidi.p@gmail.com>
2018-06-11 20:02:34 +0200

    [ADD] Navigation

--- resources/views/partials/left.blade.php
+++ resources/views/partials/left.blade.php
@@ -2,10 +2,10 @@
     <div>
         <new-conversation-modal></new-conversation-modal>
         <div class="main">
-            <a href="{{ route('chat.index') }}" class="item active"><span class="icon"><i class="fa fa-envelope-o"></i></span><span class="name">Chat</span></a>
-            <a href="{{ route('profile.index') }}" class="item"><span class="icon"><i class="fa fa-user"></i></span><span class="name">Profile</span></a>
-            <a href="{{ route('friends.index') }}" class="item"><span class="icon"><i class="fa fa-group"></i></span><span class="name">Friends</span></a>
-            <a href="{{ route('users.all') }}" class="item"><span class="icon"><i class="fa fa-group"></i></span><span class="name">Users</span></a>
+            <a href="{{ route('chat.index') }}" class="item {{ Nav::isRoute('chat.index') }}"><span class="icon"><i class="fa fa-envelope-o"></i></span><span class="name">Chat</span></a>
+            <a href="{{ route('profile.index') }}" class="item {{ Nav::isRoute('profile.index') }}"><span class="icon"><i class="fa fa-user"></i></span><span class="name">Profile</span></a>
+            <a href="{{ route('friends.index') }}" class="item {{ Nav::isRoute('friends.index') }}"><span class="icon"><i class="fa fa-group"></i></span><span class="name">Friends</span></a>
+            <a href="{{ route('users.all') }}" class="item {{ Nav::isRoute('users.all') }}"><span class="icon"><i class="fa fa-group"></i></span><span class="name">Users</span></a>
             <a href="#" class="item"><span class="icon"><i class="fa fa-cog"></i></span><span class="name">Settings</span></a>
         </div>
     </div>
